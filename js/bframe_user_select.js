f643029a00c25b43228a155e8e9ebf61de4d6240
Takao Nishida <nishida@bigbeat.co.jp>
2019-03-22 18:32:14 +0900

    fixed

--- js/bframe_user_select.js
+++ js/bframe_user_select.js
@@ -29,9 +29,12 @@
 
 		add_button.addEventListener('click', add);
 		del_button.addEventListener('click', del);
+		document.body.addEventListener('click', reset);
 
 		function selectable_table(table) {
 			var last_row;
+
+			document.addEventListener('keydown', onKeydown);
 
 			for(let i=0; i < table.rows.length; i++) {
 				table.rows[i].addEventListener('click', onClick);
@@ -40,35 +43,64 @@
 			function onClick(event) {
 				if(event.button == 2) return;
 
+				resetSelectionToSelected(table);
+
+				for(let i=0; i < table.rows.length; i++) {
+					table.rows[i].classList.remove('selected');
+				}
+
 				if(event.ctrlKey || event.metaKey) {
-					last_row = this.rowIndex;
-					this.classList.toggle('selected');
+					this.classList.toggle('current');
 				}
 				else if(event.shiftKey) {
-					if(last_row != 'undefined') {
+					if(last_row != 'undefined' || last_row == '') {
 						if(last_row < this.rowIndex) {
 							for(let i=last_row; i <= this.rowIndex; i++) {
-								table.rows[i].classList.add('selected');
+								table.rows[i].classList.add('current');
 							}
 						}
 						else {
 							for(let i=this.rowIndex; i <= last_row; i++) {
-								table.rows[i].classList.add('selected');
+								table.rows[i].classList.add('current');
 							}
 						}
 					}
 				}
 				else {
 					for(let i=0; i < table.rows.length; i++) {
-						table.rows[i].classList.remove('selected');
+						table.rows[i].classList.remove('current');
 					}
 					last_row = this.rowIndex;
-					this.classList.toggle('selected');
+					this.classList.toggle('current');
 				}
+				last_row = this.rowIndex;
 			}
 
 			function onKeydown(event) {
-console.log('onKeydown');
+				if(last_row === 'undefined' || last_row === '') return;
+
+				if(window.event) {
+					keycode = window.event.keyCode;
+				}
+				else {
+					keycode = event.keyCode;
+				}
+
+				switch(keycode) {
+				case 38:	// up
+					if(last_row > 0) {
+						if(!event.shiftKey) resetSelection();
+						table.rows[--last_row].classList.add('current');
+					}
+					break;
+
+				case 40:	// down
+					if(last_row < table.rows.length-1) {
+						if(!event.shiftKey) resetSelection();
+						table.rows[++last_row].classList.add('current');
+					}
+					break;
+				}
 			}
 
 			this.getAllRow = function() {
@@ -85,7 +117,7 @@ console.log('onKeydown');
 				var collection = [];
 
 				for(let i=0, index=0; i < table.rows.length; i++) {
-					if(table.rows[i].classList.contains('selected')) {
+					if(table.rows[i].classList.contains('current') || table.rows[i].classList.contains('selected')) {
 						collection[index++] = table.rows[i];
 					}
 				}
@@ -97,12 +129,15 @@ console.log('onKeydown');
 				for(let i=0; i < collection.length; i++) {
 					for(var j=0, index=0; j < table.rows.length; j++) {
 						if(collection[i].cells[1].innerHTML == table.rows[j].cells[1].innerHTML) {
+							table.rows[j].classList.add('selected');
 							break;
 						}
 					}
 					if(j == table.rows.length) {
 						var tr = collection[i].cloneNode(true);
 						tr.addEventListener('click', onClick);
+						tr.classList.remove('current');
+						tr.classList.add('selected');
 						table.appendChild(tr);
 					}
 				}
@@ -112,18 +147,53 @@ console.log('onKeydown');
 				var collection = [];
 
 				for(let i=0; i < table.rows.length;) {
-					if(table.rows[i].classList.contains('selected')) {
+					if(table.rows[i].classList.contains('current') || table.rows[i].classList.contains('selected')) {
 						table.deleteRow(i);
 						continue;
 					}
 					i++;
 				}
 			}
+
+			this.setSelectionToSelected = function() {
+				for(let i=0, index=0; i < table.rows.length; i++) {
+					if(table.rows[i].classList.contains('current')) {
+						table.rows[i].classList.remove('current');
+						table.rows[i].classList.add('selected');
+					}
+				}
+				last_row = '';
+			}
+
+			function resetSelection() {
+				for(let i=0, index=0; i < table.rows.length; i++) {
+					table.rows[i].classList.remove('current');
+					table.rows[i].classList.remove('selected');
+				}
+			}
+			this.resetSelection = resetSelection;
+		}
+
+		function reset(event) {
+			resetSelectionToSelected();
+		}
+
+		function resetSelectionToSelected(table) {
+			if(!table || table != right_table) {
+				right.setSelectionToSelected();
+			}
+			if(!table || table != left_table) {
+				left.setSelectionToSelected();
+			}
 		}
 
 		function add(event) {
 			var collection = right.getSelectedRow();
 			left.addRow(collection);
+
+			right.setSelectionToSelected();
+			left.setSelectionToSelected();
+
 			bframe.stopPropagation(event);
 		}
 
