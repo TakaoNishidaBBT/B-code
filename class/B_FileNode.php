4fedae2f4da1b9ad90c053f0f0a7d6ebda0f2a3c
Takao Nishida <nishida@bigbeat.co.jp>
2019-07-22 18:37:31 +0900

    fixed

--- class/B_FileNode.php
+++ class/B_FileNode.php
@@ -566,6 +566,12 @@
 			}
 		}
 
+		function removethumbnail() {
+			if($this->thumb && file_exists($this->thumb)) {
+				unlink($this->thumb);
+			}
+		}
+
 		function getThumbnailImgPath($path) {
 			if(substr($path, 0, 1) == '/') $path = substr($path, 1);
 
